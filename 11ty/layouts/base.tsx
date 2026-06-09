4730b9c365c7d0cae14a1f0370692bf90c46dcbc
Jon Pearse <hello@jonpearse.net>
2026-06-09 14:44:51 +0100

    Implement very basic critical CSS

--- 11ty/layouts/base.tsx
+++ 11ty/layouts/base.tsx
@@ -40,6 +40,7 @@ export default ( { title, description, content, nav, navKey, eleventy, meta, pag
 				<meta name="fediverse:creator" content="@jonpearse@front-end.social" />
 
 				{/* Assets */}
+				<style>@import "/a/critical.css";</style>
 				<link rel="stylesheet" href="/a/site.css" />
 				{/*<script src="/a/site.js" type="module" async defer />*/}
 			</head>
